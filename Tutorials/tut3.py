342726bcaeef4169b40e7ba045a293865132175c
FathimaMehrinVS <mehrinfiza57@gmail.com>
2026-03-15 23:11:20 +0530

    Added tutorial 3 Python program

---
+++ Tutorials/tut3.py
@@ -0,0 +1,12 @@
+import turtle
+t=turtle.Turtle()
+for i in range (4):
+    t.forward(100)
+    t.right(90)
+t.up()
+t.forward(200)
+t.down()
+for i in range (6):
+    t.forward(100)
+    t.right(60)
+turtle.done()
